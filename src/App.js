d7de066f61f6d4b667f121050cef0e1d18e2b323
ShavelDev <nor.szaw@gmail.com>
2024-08-15 17:30:08 +0200

    initial commit, forms not working yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState} from 'react';
 import './App.css';
 
+
+
+
 function App() {
+
+  const [notes, changeNotes] = useState([]);
+
+  const addNote = () => {
+
+    changeNotes( (prevNotes) => [...prevNotes, {title: "TITLE", noteContent: "CONTENT"}])
+
+  }
+
+  
+
+  const hideForm = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navbar'>navbar
+      <div className='button' onClick={addNote}> Add Note</div>
+      </nav>
+      <div className='mainSection'>
+
+
+    <Notes notes = {notes}></Notes>
+
+      <div className='formBox'>
+      <form>
+        
+      </form>
+
+
+      </div>
+
+      </div>
+      <footer>footer</footer>
     </div>
   );
+}
+
+function Notes(props){
+
+  
+
+ const notesJSX = props.notes.map(note => (<Note noteContent = {note.noteContent} title = {note.title} ></Note>))
+
+  return(
+
+    <div className='NotesBox'>
+      {notesJSX}
+    </div>
+
+  );
+}
+
+function Note(props){
+
+  return(
+    <div className='Note'>
+      <p>{props.title}</p>  
+      <p>props.noteContent</p>
+    </div>
+  );
+
 }
 
 export default App;
